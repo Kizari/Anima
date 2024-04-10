59e35d30dba47cd63cd0b17ed4e8c4af55306192
Kizari <25322543+Kizari@users.noreply.github.com>
2024-04-10 14:16:34 +1200

    test: add integration tests for source generators
    Directory.Build.props was moved into src folder to prevent adding NuGet metadata to test projects. Local NuGet source was added to integration test the source generators. This requires updating the <Version> tag in Directory.Build.props, running dotnet pack, and updating the version in the integration tests csproj files. CI/CD workflow has been updated to pack the packages locally for integration testing.

---
+++ test/Anima.DependencyInjection.SourceGeneration.IntegrationTests/RegisterServiceGeneratorTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Anima.DependencyInjection.SourceGeneration.IntegrationTests;
+
+public class RegisterServiceGeneratorTests
+{
+    [Fact]
+    public void Generator_WhenExtensionCalled_RegistersServicesCorrectly()
+    {
+        var collection = new ServiceCollection()
+            .AddAnimaDependencyInjectionSourceGenerationIntegrationTests();
+        
+        var scoped = collection.FirstOrDefault(d => d.ServiceType == typeof(TestScopedService));
+        Assert.NotNull(scoped);
+        Assert.Equal(ServiceLifetime.Scoped, scoped.Lifetime);
+        
+        var singleton = collection.FirstOrDefault(d => d.ServiceType == typeof(TestSingletonService));
+        Assert.NotNull(singleton);
+        Assert.Equal(ServiceLifetime.Singleton, singleton.Lifetime);
+        
+        var transient = collection.FirstOrDefault(d => d.ServiceType == typeof(TestTransientService));
+        Assert.NotNull(transient);
+        Assert.Equal(ServiceLifetime.Transient, transient.Lifetime);
+    }
+}
+
+[RegisterService(ServiceLifetime.Scoped)]
+public class TestScopedService;
+
+[RegisterService(ServiceLifetime.Singleton)]
+public class TestSingletonService;
+
+[RegisterService(ServiceLifetime.Transient)]
+public class TestTransientService;
---
+++ test/Anima.EntityFrameworkCore.SourceGeneration.IntegrationTests/DbSetGeneratorTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Anima.EntityFrameworkCore.SourceGeneration.IntegrationTests;
+
+public class DbSetGeneratorTests
+{
+    [Fact]
+    public void Generator_CreatesDbSets()
+    {
+        using var context = new TestContext();
+        Assert.Empty(context.TestEntity1s);
+        Assert.Empty(context.TestEntity2s);
+    }
+}
+
+[GenerateDbSets]
+public partial class TestContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+    }
+}
+
+public class TestEntity1 : IEntity
+{
+    public int Id { get; set; }
+}
+
+public class TestEntity2 : IEntityTypeConfiguration<TestEntity2>
+{
+    public int Id { get; set; }
+    
+    public void Configure(EntityTypeBuilder<TestEntity2> builder) { }
+}
